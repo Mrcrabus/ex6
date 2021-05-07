31ca4693bffd110287ac3020fc5a36bb56aa62bf
svonk <example@mail.ru>
2021-05-07 09:49:57 +0300

    Pre axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import s from './users.module.css'
 
-let Users =(props) => {
+let Users = (props) => {
     return <div>
-            <h1>Here</h1>
+        <h1>Here</h1>
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.avatar} className={s.photo} alt="..."/>
+                    </div>
+                    <div>
+                        {u.last_name}
+                    </div>
+                    <div>
+                        {u.first_name}
+                    </div>
+                </span>
+            </div>)
+        }
     </div>
 }
 
