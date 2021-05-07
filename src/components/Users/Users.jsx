65575cc4b26fcd918754078726c33b5621cea80f
svonk <example@mail.ru>
2021-05-07 10:37:34 +0300

    Get Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import s from './users.module.css'
+import * as axios from 'axios';
 
 let Users = (props) => {
+
+    let getUsers = () =>
+    {
+        if (props.users.length === 0) {
+            axios.get("https://reqres.in/api/users")
+                .then(response => {
+                    debugger;
+                    props.setUsers(response.data.data);
+                });
+        }
+    }
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         <h1>Here</h1>
         {
             props.users.map(u => <div key={u.id}>
