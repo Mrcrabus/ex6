5c3ab5a11a53770e19bfe6736b2b20a2b294158f
svonk <example@mail.ru>
2021-05-07 08:27:10 +0300

    First commit

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+// import * as axios from "axios";
+//
+// // export const usersAPI = {
+// //     getUsers() {
+// //         return axios.get("https://reqres.in/api/users")
+// //             .then(response => {
+// //                 response.data
+// //             })
+// //     }
+// }
